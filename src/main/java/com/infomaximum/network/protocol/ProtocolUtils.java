ea9b316207ddab3e9918df1a4797fdf18459305a
Vladimir A. Ulitin <ulitin@infomaximum.com>
2020-10-18 16:15:07 +0300

    increases version to: 0.0.18

--- src/main/java/com/infomaximum/network/protocol/ProtocolUtils.java
+++ src/main/java/com/infomaximum/network/protocol/ProtocolUtils.java
@@ -1,11 +1,16 @@
 package com.infomaximum.network.protocol;
 
+import org.eclipse.jetty.websocket.api.UpgradeRequest;
 import org.eclipse.jetty.websocket.common.WebSocketSession;
 
 public class ProtocolUtils {
 
     public static String getWebSocketProtocol(WebSocketSession webSocketSession) {
-        String protocol = webSocketSession.getUpgradeRequest().getHeader("Sec-WebSocket-Protocol");
+        return getWebSocketProtocol(webSocketSession.getUpgradeRequest());
+    }
+
+    public static String getWebSocketProtocol(UpgradeRequest upgradeRequest) {
+        String protocol = upgradeRequest.getHeader("Sec-WebSocket-Protocol");
         return protocol;
     }
 }
